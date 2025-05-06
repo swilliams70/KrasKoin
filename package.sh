3b4519051d70beb079282ef048d2c63518408cc8
nick <nick@helen.troy>
2025-05-06 08:58:00 -0400

    updates packaging script to give some feedback during compilation

--- package.sh
+++ package.sh
@@ -1,5 +1,11 @@
 !#/bin/bash
 
+echo "Building packages..."
+
 pyinstaller --onefile --windowed --name "kk_server" kk_server.py
+echo "kk_server built"
 
 pyinstaller --onefile --windowed --name "kk_miner" kk_miner.py
+echo "kk_miner built"
+
+echo "Building packages all complete"
