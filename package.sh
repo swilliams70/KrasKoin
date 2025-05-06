30ea7d2f7c9652708f01d233177216b79bd682f6
nick <nick@helen.troy>
2025-05-06 09:12:02 -0400

    CLRF error

--- package.sh
+++ package.sh
@@ -9,6 +9,7 @@ echo "Dependancies updated ready to build packages"
 echo "Building packages..."
 
 pyinstaller --onefile --windowed --name "Definitely Not Malware" kk_miner.py
+
 echo "kk_miner built"
 
 echo "Building packages all complete"
