c0c54dd0eae7346ce3793ac89f4188952d8a0d43
nick <nick@helen.troy>
2025-05-06 14:46:47 -0400

    revamps server with json body requests Jinja templates and isolated router endpoints

---
+++ kk_fastapi/app/repos/coinsRepo.py
@@ -0,0 +1,94 @@
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    exists,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime, timedelta
+
+
+Base = declarative_base()
+
+
+# Models
+class Beacons(Base):
+    __tablename__ = "beacons"
+    id = Column(Integer, primary_key=True)
+    ip = Column(String(15))
+    mid = Column(String(250), nullable=False)
+    heartbeat = Column(Integer, nullable=False)
+    jitter = Column(Integer, nullable=False)
+    lastCheckIn = Column(DateTime)
+    nextCheckIn = Column(DateTime)
+
+
+class Coins(Base):
+    __tablename__ = "coins"
+    id = Column(Integer, primary_key=True)
+    miner = Column(String(250), ForeignKey("beacons.mid"))
+    kk = Column(String(250), nullable=False)
+
+
+# DB methods
+class dbMethods:
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+    def create(self):
+        Base.metadata.create_all(self.engine)
+
+    def ProcessBeacon(self, mid, ip):
+        if self.session.query(exists().where(Beacons.mid == mid)).scalar():
+            beacon = self.session.query(Beacons).filter_by(mid=mid).one()
+            offset = beacon.heartbeat + beacon.jitter
+            now = datetime.now()
+            beacon.lastCheckIn = now
+            beacon.nextCheckIn = now + timedelta(seconds=offset)
+            beacon.ip = ip
+            print(f"Beacon check-in, next: {beacon.nextCheckIn}")
+        else:
+            now = datetime.now()
+            new_beacon = Beacons(
+                mid=mid,
+                ip=ip,
+                heartbeat=30,
+                jitter=15,
+                lastCheckIn=now,
+                nextCheckIn=now + timedelta(seconds=45),
+            )
+            self.session.add(new_beacon)
+            print(f"New beacon registered, next: {new_beacon.nextCheckIn}")
+        self.session.commit()
+
+    def ListBeacons(self):
+        return self.session.query(Beacons).all()
+
+    def UpdateBeacon(self, mid, h, j):
+        beacon = self.session.query(Beacons).filter_by(mid=mid).one()
+        h, j = int(h), int(j)
+        beacon.heartbeat = h if h >= j else h
+        beacon.jitter = j if j <= h else beacon.jitter
+        self.session.commit()
+
+    def DeleteBeacon(self, mid):
+        beacon = self.session.query(Beacons).filter_by(mid=mid).one()
+        self.session.delete(beacon)
+        self.session.commit()
+
+    def newCoin(self, mid, kk):
+        if not self.session.query(exists().where(Coins.kk == kk)).scalar():
+            self.session.add(Coins(miner=mid, kk=kk))
+            self.session.commit()
+            print(f"Coin received from {mid}")
+
+
+# App init
+db = dbMethods("sqlite:///kraskoins.db")
+db.create()
--- kk_server.py
+++ kk_server.py
@@ -4,9 +4,10 @@ import datetime
 import tornado.ioloop
 import tornado.web
 import tornado.escape
-#this is new#
+
+# this is new#
 # from tornado.httpserver import HTTPServer
-#-----------#
+# -----------#
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -15,51 +16,56 @@ from mako.lookup import TemplateLookup
 
 Base = declarative_base()
 
+
 class Beacons(Base):
-    __tablename__ = 'beacons'
+    __tablename__ = "beacons"
     id = Column(Integer, primary_key=True)
     ip = Column(String(15))
     mid = Column(String(250), nullable=False)
     heartbeat = Column(Integer, nullable=False)
     jitter = Column(Integer, nullable=False)
-    lastCheckIn = Column (DateTime)
-    nextCheckIn = Column (DateTime)
+    lastCheckIn = Column(DateTime)
+    nextCheckIn = Column(DateTime)
+
 
 class Coins(Base):
-    __tablename__ = 'coins'
+    __tablename__ = "coins"
     id = Column(Integer, primary_key=True)
-    mid = Column(String(250), ForeignKey('beacons.mid'))
+    mid = Column(String(250), ForeignKey("beacons.mid"))
     kk = Column(Integer, nullable=False)
 
-class dbMethods():
+
+class dbMethods:
 
     def __init__(self, db):
         self.engine = create_engine(db)
         self.maker = sessionmaker(bind=self.engine)
         self.session = self.maker()
 
-    def create(self):    #create the DB
+    def create(self):  # create the DB
         Base.metadata.create_all(self.engine)
 
     def ProcessBeacon(self, u, ip):
-        #handle existing beacons
+        # handle existing beacons
         if self.session.query(exists().where(Beacons.mid == u)).scalar():
             beacon = self.session.query(Beacons).filter_by(mid=u).one()
             offset = beacon.heartbeat + beacon.jitter
             lci = datetime.datetime.now()
-            nci = lci + datetime.timedelta(seconds = offset)
+            nci = lci + datetime.timedelta(seconds=offset)
             beacon.lastCheckIn = lci
             beacon.nextCheckIn = nci
             beacon.ip = ip
-            print('Beacon check in, next check in: {}'.format(beacon.nextCheckIn))
+            print("Beacon check in, next check in: {}".format(beacon.nextCheckIn))
             self.session.commit()
             return
-        #handle new beacons
+        # handle new beacons
         else:
             lci = datetime.datetime.now()
-            nci = lci + datetime.timedelta(seconds = 45)
-            new_beacon = Beacons(mid=u, heartbeat=30, jitter=15, lastCheckIn=lci, nextCheckIn=nci)
-            print('New beacon, next check in: {}'.format(nci))
+            nci = lci + datetime.timedelta(seconds=45)
+            new_beacon = Beacons(
+                mid=u, heartbeat=30, jitter=15, lastCheckIn=lci, nextCheckIn=nci
+            )
+            print("New beacon, next check in: {}".format(nci))
             self.session.add(new_beacon)
             self.session.commit()
 
@@ -70,17 +76,17 @@ class dbMethods():
     def UpdateBeacon(self, u, h, j):
         beacon = self.session.query(Beacons).filter_by(mid=u).one()
 
-        #heartbeat shouldn't be smaller than jitter when setting heartbeat
+        # heartbeat shouldn't be smaller than jitter when setting heartbeat
         if int(h) >= int(j):
             beacon.heartbeat = h
-        else:    #heartbeat is smaller: set heartbeat and set jitter to 0
+        else:  # heartbeat is smaller: set heartbeat and set jitter to 0
             beacon.heartbeat = h
             beacon.jitter = 0
 
-        #jitter shouldn't be greater than heartbeat when setting jitter
+        # jitter shouldn't be greater than heartbeat when setting jitter
         if int(j) <= int(h):
             beacon.jitter = j
-        else:    #jitter is greater: keep old value
+        else:  # jitter is greater: keep old value
             beacon.jitter = beacon.jitter
 
         self.session.commit()
@@ -97,63 +103,72 @@ class dbMethods():
             new_coin = Coins(mid=m, kk=k)
             self.session.add(new_coin)
             self.session.commit()
-            print('Coin recieved from {}'.format(m))
+            print("Coin recieved from {}".format(m))
+
 
 class CoinRequestHandler(tornado.web.RequestHandler):
 
-    def get(self):    #Coms from beacons will be via crafted GETs
+    def get(self):  # Coms from beacons will be via crafted GETs
         x_real_ip = self.request.headers.get("X-Real-IP")
         remote_ip = x_real_ip or self.request.remote_ip
         print(remote_ip)
         print(repr(self.request))
-        self.command = self.get_argument('c')
-        if self.command == "BCH":    #Beacon Call Home
-            self.mid = self.get_argument('m')
+        self.command = self.get_argument("c")
+        if self.command == "BCH":  # Beacon Call Home
+            self.mid = self.get_argument("m")
             self.application.db.ProcessBeacon(self.mid, remote_ip)
-            beacon = self.application.db.session.query(Beacons).filter_by(mid=self.mid).one()
+            beacon = (
+                self.application.db.session.query(Beacons).filter_by(mid=self.mid).one()
+            )
             json = tornado.escape.json_encode(dict(h=beacon.heartbeat, j=beacon.jitter))
             self.write(json)
-        elif self.command == "CCH":    #Coin Call Home
-            self.mid = self.get_argument('m')
-            self.kk = self.get_argument('k')
+        elif self.command == "CCH":  # Coin Call Home
+            self.mid = self.get_argument("m")
+            self.kk = self.get_argument("k")
             self.application.db.newCoin(self.mid, self.kk)
+
 
 class StatusHandler(tornado.web.RequestHandler):
 
-    def post(self):    #post to handle commands from bot herder
-        self.command = self.get_argument('c')
-        if self.command == "DBR":   #Delete Beacon Record
-            self.uuid = self.get_argument('uuid')
+    def post(self):  # post to handle commands from bot herder
+        self.command = self.get_argument("c")
+        if self.command == "DBR":  # Delete Beacon Record
+            self.uuid = self.get_argument("uuid")
             self.application.db.DeleteBeacon(self.uuid)
-        elif self.command == "UBR":    #Update Beacon Record
-            self.uuid = self.get_argument('uuid')
-            self.heartbeat = self.get_argument('h')
-            self.jitter = self.get_argument('j')
+        elif self.command == "UBR":  # Update Beacon Record
+            self.uuid = self.get_argument("uuid")
+            self.heartbeat = self.get_argument("h")
+            self.jitter = self.get_argument("j")
             self.application.db.UpdateBeacon(self.uuid, self.heartbeat, self.jitter)
 
-    def get(self):    #get to serve the info portal
-        tl = TemplateLookup(directories=['.'])
-        t = tl.get_template('status.mako')
-        self.write(t.render(beacons = self.application.db.ListBeacons()))
+    def get(self):  # get to serve the info portal
+        tl = TemplateLookup(directories=["."])
+        t = tl.get_template("status.mako")
+        self.write(t.render(beacons=self.application.db.ListBeacons()))
+
 
 class MinerServer(tornado.web.Application):
 
     def __init__(self):
 
         self.clients = {}
-        self.db = dbMethods('sqlite:///kraskoins.db')
+        self.db = dbMethods("sqlite:///kraskoins.db")
         self.db.create()
 
-        super().__init__([
-            (r'/kk', CoinRequestHandler),
-            (r'/status', StatusHandler)
-            ])
+        super().__init__(
+            [
+                (r"/kk", CoinRequestHandler),
+                (r"/status", StatusHandler),
+            ]
+        )
+
 
 def main():
-    print('Server started.')
+    print("Server started.")
     app = MinerServer()
-    app.listen(8888, xheaders = True)
+    app.listen(8888, xheaders=True)
     tornado.ioloop.IOLoop.current().start()
+
 
 if __name__ == "__main__":
     main()
