3b0c36ef576036edf9019a6e50fad7010883c89c
nick <nick@helen.troy>
2025-05-06 09:07:31 -0400

    Improves the packaging script

--- package.sh
+++ package.sh
@@ -8,9 +8,6 @@ echo "Dependancies updated ready to build packages"
 
 echo "Building packages..."
 
-pyinstaller --onefile --windowed --name "kk_server" kk_server.py
-echo "kk_server built"
-
 pyinstaller --onefile --windowed --name "kk_miner" kk_miner.py
 echo "kk_miner built"
 
